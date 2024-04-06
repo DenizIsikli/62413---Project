c86c437ce522e13fe2e7bd97fc424f9a2f92f021
VyTrinh <Jackie_Trinh@hotmail.com>
2024-04-06 21:29:05 +0200

    Addded multiple page navigation.

---
+++ 62413 - Project/LoginScreen.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _62413___Project
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Page
+    {
+        public string nameInsert = "";
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void NameChange(object sender, RoutedEventArgs e)
+        {
+            nameInsert = LoginBox.Text;
+            ChatScreen p = new ChatScreen();
+            this.NavigationService.Navigate(p, nameInsert);
+        }
+
+        public string GetLoginName()
+        {
+            return nameInsert;
+        }
+    }
+}
--- 62413 - Project/MainWindow.xaml.cs
+++ 62413 - Project/MainWindow.xaml.cs
@@ -8,89 +8,13 @@ namespace _62413___Project
 {
     public partial class MainWindow : Window
     {
-        private readonly Client client = new();
         private readonly ThemeSwitch _themeSwitch = new();
-        public ObservableCollection<ChatMessage> ChatMessages { get; set; }
 
         public MainWindow()
         {
             _themeSwitch.ApplyTheme("LightTheme.xaml");
             InitializeComponent();
-            Task.Run(() => StartServer());
-            ChatMessages = new ObservableCollection<ChatMessage>();
-            listBoxChat.ItemsSource = ChatMessages;
-            client.MessageReceived += Client_MessageReceived;
-            client.Connect("127.0.0.1", 8888);
-        }
-        
-        /// <summary>
-        /// Starts the server on a separate thread.
-        /// </summary>
-        private void StartServer()
-        {
-            Server server = new();
-            server.Start(8888);
-        }
-
-        /// <summary>
-        /// Button click event to send a message to the server.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ButtonSend_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(textBoxMessage.Text)) return;
-            textBoxMessage.Focus();
-            client.SendMessage(textBoxMessage.Text);
-            textBoxMessage.Clear();
-        }
-
-        /// <summary>
-        /// Textbox key down event to send a message to the server when the enter key is pressed.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void TextBoxMessage_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter && !string.IsNullOrEmpty(textBoxMessage.Text))
-            {
-                client.SendMessage(textBoxMessage.Text);
-                textBoxMessage.Clear();
-            }
-        }
-
-        /// <summary>
-        /// Button click event to disconnect the client from the server.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ButtonDisconnect_Click(object sender, RoutedEventArgs e)
-        {
-            client.Disconnect();
-        }
-
-        /// <summary>
-        /// Event handler for when a message is received from the server.
-        /// </summary>
-        /// <param name="message"></param>
-        private void Client_MessageReceived(string message)
-        {
-            var messageParts = message.Split([':'], 2);
-            if (messageParts.Length == 2)
-            {
-                var chatMessage = new ChatMessage
-                {
-                    Name = messageParts[0].Trim(),
-                    Message = messageParts[1].Trim(),
-                    Timestamp = DateTime.Now.ToString("HH:mm:ss"),
-                };
-
-                Dispatcher.Invoke(() =>
-                {
-                    ChatMessages.Add(chatMessage);
-                    listBoxChat.ScrollIntoView(chatMessage);
-                });
-            }
+            Main.Content = new LoginScreen(); /// Set LoginScreen as start page
         }
 
         /// <summary>
@@ -112,15 +36,5 @@ namespace _62413___Project
         {
             _themeSwitch.ApplyTheme("LightTheme.xaml");
         }
-    }
-
-    /// <summary>
-    /// Represents a chat message.
-    /// </summary>
-    public class ChatMessage
-    {
-        public string? Name { get; set; }
-        public string? Message { get; set; }
-        public string? Timestamp { get; set; }
     }
 }
