63c3a0eb5e9c3015780139ff2a70e0491544c2b0
Bobby <s215818@dtu.dk>
2024-05-01 18:35:55 +0200

    Removed theme  toggle button from main.xaml and put it in chatscreen.xaml

--- 62413 - Project/MainWindow.xaml.cs
+++ 62413 - Project/MainWindow.xaml.cs
@@ -18,25 +18,5 @@ namespace _62413___Project
             InitializeComponent();
             Main.Content = new Lobby();
         }
-
-        /// <summary>
-        /// Event handler for when the theme toggle is checked.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ThemeToggle_Checked(object sender, RoutedEventArgs e)
-        {
-            _themeSwitch.ApplyTheme("DarkTheme.xaml");
-        }
-
-        /// <summary>
-        /// Event handler for when the theme toggle is unchecked.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ThemeToggle_Unchecked(object sender, RoutedEventArgs e)
-        {
-            _themeSwitch.ApplyTheme("LightTheme.xaml");
-        }
     }
 }
